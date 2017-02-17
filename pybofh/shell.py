75ad6304587b9bcabebc654be48715c7526d07bc
goncalopp <goncalopp@users.noreply.github.com>
2017-02-17 09:43:11 +0000

    FIXUP shell

--- pybofh/shell.py
+++ pybofh/shell.py
@@ -1,4 +1,7 @@
 import subprocess
+import logging
+
+log = logging.getLogger(__name__)
 
 def check_call(command):
     run_process(command)
@@ -6,7 +9,8 @@ def check_call(command):
 def check_output(command):
     return run_process(command)
 
-def run_subprocess(command):
+def run_process(command):
+    logging.debug("Running command: %s", lambda: "".join(command))
     result = subprocess.check_output(command)
     return result
 
