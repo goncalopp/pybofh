7236ea768374923a3ccf5d1484a1a8bae6468c7e
goncalopp <goncalopp@users.noreply.github.com>
2018-02-07 18:29:00 +0000

    __init__.py: fix internal imports

--- pybofh/__init__.py
+++ pybofh/__init__.py
@@ -1,14 +1,14 @@
-import settingsmodule
+from . import settingsmodule
 # forward declaration, so package modules can use it
 settings = settingsmodule.Settings()
 
 from functools import partial
 
-import blockdevice
-import mount
-import filesystem
-import btrfs
-import xen
-import lvm
-import drbd
-from atomic_operations import AtomicContext
+from . import blockdevice
+from . import mount
+from . import filesystem
+from . import btrfs
+from . import xen
+from . import lvm
+from . import drbd
+from .atomic_operations import AtomicContext
