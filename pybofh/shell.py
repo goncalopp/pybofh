4dddc6b593524eb4cabae8b3ccdcc61462c2140f
root <root@hp-elitebook-840>
2018-02-07 12:44:23 +0000

    shell: better logging, typed exception, doc, MockShell.add_mock_binary

--- pybofh/shell.py
+++ pybofh/shell.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
+import functools
 import logging
+import os.path
 import subprocess
 
 log = logging.getLogger(__name__)
@@ -21,8 +23,14 @@ class Shell(object):
             command = (command,)
         else:
             command = tuple(command)
-        logging.debug("Running command: %s", lambda: "".join(command))
-        return self._run_process(command)
+        logging.debug("Running command: %s" % " ".join(command))
+        try:
+            response = self._run_process(command)
+        except Exception as e:
+            logging.debug("Failed: %s" % e)
+            raise
+        logging.debug("Result: %s" % response)
+        return response
 
     def _run_process(self, command):
         raise NotImplementedError
@@ -33,12 +41,18 @@ class SystemShell(Shell):
         return result
 
 class MockShell(Shell):
+    class NoMockForCommand(Exception):
+        def __init__(self, command):
+            Exception.__init__(self, "No mock exists for command: {}".format(command))
+
     def __init__(self):
         self._mocks = []
         self.run_commands = []
 
     def add_mock(self, command, response):
-        """Command can either be a tuple, or a callable.
+        """Adds a mock command to the MockShell.
+
+        Command can either be a tuple, or a callable.
         If it's a tuple, it represents the literal command that this mock should respond to.
         Example: ("ls", "thisdirectory")
         If command is a callable, it should take a single argument (the command) and return True iff the response should be used.
@@ -55,16 +69,53 @@ class MockShell(Shell):
             command = tuple(command)
         self._mocks.append((command, response))
 
+    def add_mock_binary(self, binary_path, response):
+        """Adds a mock "binary" to the MockShell.
+
+        This is a helper function on top of add_mock(), that helps creating the command function,
+        such that all commands that start with the given binary name are matched. This also simulates shell path, so that add_mock_binary('/bin/ls', ...) will match command ('ls', ...).
+        binary_path can be an absolute or relative path.
+        The response argument is passed unmodified to add_mock() (see documentation there).
+        """
+        f = functools.partial(_command_matches_binary, binary_path)
+        self.add_mock(f, response)
+
     def _get_mock_response(self, command):
         for mock_command, mock_response in self._mocks:
             if (callable(mock_command) and mock_command(command)) or command == mock_command:
                 self.run_commands.append(command)
                 return mock_response(command) if callable(mock_response) else mock_response
-        raise Exception("No mock response found for {}".format(command))
+        raise MockShell.NoMockForCommand(command)
 
     def _run_process(self, command):
         assert not isinstance(command, str)
         return self._get_mock_response(command)
+
+def _command_matches_binary(binary, command):
+    """Evaluates whether a given command matches a given binary.
+    Example: _command_matches_binary('/bin/ls', ('ls', 'mydir')) == True
+    """
+    assert isinstance(binary, str)
+    absolute_binary = binary.startswith('/')
+    absolute_command = command[0].startswith('/')
+    if absolute_binary:
+        if absolute_command:
+            return binary == command[0]
+        else: # absolute_binary, not absolute_command
+            path = ('/bin', '/sbin', '/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin')
+            dirname, binname = os.path.split(binary)
+            return dirname in path and command[0] == binname
+    else: # not absolute_binary
+        if absolute_command:
+            return False # local binaries are not added to any directory (i.e.: no simulated cwd)
+        else: #not absolute_binary, not absolute_command
+            return binary == command[0]
+    assert False # should not be reached
+
+
+
+
+    raise NotImplementedError
 
 shell = SystemShell() # singleton
 
--- pybofh/tests/test_shell.py
+++ pybofh/tests/test_shell.py
@@ -21,33 +21,56 @@ class SystemShellTest(unittest.TestCase):
 class MockShellTest(unittest.TestCase):
     def test_check_call(self):
         shell = MockShell()
-        shell.add_mock(('pwd'), 'response')
-        shell.add_mock(lambda command: command[0] == 'who', 'user')
-        shell.add_mock(
-                lambda command: command[0] == ('ls'), 
-                lambda command: 'response ' + command[1])
+        shell.add_mock('pwd', 'response')
         response = shell.check_call(('pwd',))
         self.assertEqual(response, None)
-        response = shell.check_call(('ls', '.'))
-        self.assertEqual(response, None)
-        with self.assertRaises(Exception):
-            # no mock for this command
-            shell.check_call(('ps', '.'))
 
     def test_check_output(self):
         shell = MockShell()
-        shell.add_mock(('pwd'), 'response')
-        shell.add_mock(lambda command: command[0] == 'who', 'user')
-        shell.add_mock(
-                lambda command: command[0] == ('ls'), 
-                lambda command: 'response ' + command[1])
+        shell.add_mock('pwd', 'response')
         response = shell.check_output(('pwd',))
         self.assertEqual(response, 'response')
-        response = shell.check_output(('ls', '.'))
-        self.assertEqual(response, 'response .')
-        with self.assertRaises(Exception):
-            # no mock for this command
-            shell.check_output(('ps', '.'))
+
+    def test_add_mock(self):
+        shell = MockShell()
+        # literal command, literal response
+        shell.add_mock(('pwd'), 'response')
+        self.assertEqual(shell.run_process('pwd',), 'response')
+        # command function, literal response
+        shell.add_mock(lambda command: command[0] == 'cat', 'content')
+        self.assertEqual(shell.run_process(('cat', 'myfile')), 'content')
+        # command function, response function
+        shell.add_mock(
+                lambda command: command[0] == ('ls'), 
+                lambda command: 'dir ' + command[1])
+        self.assertEqual(shell.run_process(('ls', '.')), 'dir .')
+        # no mock command match
+        with self.assertRaises(MockShell.NoMockForCommand):
+            shell.run_process(('ps', '.'))
+
+    def test_add_mock_binary(self):
+        shell = MockShell()
+        # literal response, relative path binary
+        shell.add_mock_binary('bla', 'abc')
+        self.assertEqual(shell.check_output(('bla', 'a')), 'abc')
+        self.assertEqual(shell.check_output(('bla', 'a', 'b')), 'abc')
+        with self.assertRaises(MockShell.NoMockForCommand):
+            shell.check_output(('/bin/bla','a'))
+        # literal response, absolute path binary
+        shell.add_mock_binary('/bin/cat', 'content')
+        self.assertEqual(shell.check_output(('/bin/cat','a')), 'content')
+        self.assertEqual(shell.check_output(('cat','a')), 'content')
+        with self.assertRaises(MockShell.NoMockForCommand):
+            shell.check_output(('/sbin/cat','a'))
+        # response function
+        shell.add_mock_binary(
+            '/bin/ls', 
+            lambda command: "called {}: {}".format(command[0], command[1]))
+        self.assertEqual(shell.check_output(('/bin/ls','a')), 'called /bin/ls: a')
+        self.assertEqual(shell.check_output(('ls','a')), 'called ls: a')
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
